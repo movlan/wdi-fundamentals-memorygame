19a4d7f87f2d891f6d412ae7ec9cf960c1d3b097
Movlanov Bahram <movlan@Movlanovs-MacBook-Pro.local>
2020-01-16 13:08:18 -0700

    created game logic

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -1,7 +1,19 @@
 console.log("Up and running!");
-let cardOne = "queen";
-let cardTwo = "queen";
-let cardThree = "king";
-let cardFour = "king"
+let cards = ["queen", "queen", "king", "king"];
+let cardsInPlay = [];
+let cardOne = cards[0];
+let cardTwo = cards[2];
+// let cardThree = cards[2];
+// let cardFour = cards[3];
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
 console.log("User flipped " + cardOne);
-console.log("User flipped " + cardThree);
+console.log("User flipped " + cardTwo);
+if (cardsInPlay.length === 2) {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You found a match!");
+	} else {
+		alert("Sorry try again!");
+	}
+
+}
